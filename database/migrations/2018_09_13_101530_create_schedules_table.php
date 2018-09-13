49b86f0855e47dbd8af2dce416207c2b0dc3c9d2
Indras Yuda <baim.moh@gmail.com>
2018-09-13 11:17:06 +0700

    upload project aikido website

---
+++ database/migrations/2018_09_13_101530_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('schedule_title', 100);
+            $table->date('schedule_date');
+            $table->time('schedule_time_from');
+            $table->time('schedule_time_to');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule');
+    }
+}
---
+++ database/migrations/2018_09_13_104632_create_video_galleries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_gallery', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('video_title', 100);
+            $table->string('video_path', 20);
+            $table->enum('publish', ['0','1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_gallery');
+    }
+}
