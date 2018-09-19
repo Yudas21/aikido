59e00e04d7519f5025b761da5ff6201cc95098f2
Indras Yuda <baim.moh@gmail.com>
2018-09-19 16:11:45 +0700

    update sql

--- aikido_db.sql
+++ aikido_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2018 at 10:58 AM
+-- Generation Time: Sep 19, 2018 at 11:10 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.0.31
 
@@ -55,7 +55,9 @@ INSERT INTO `akses` (`id`, `menu_id`, `level_id`, `created_at`, `updated_at`) VA
 (12, 11, 2, '2018-09-17 01:26:31', '2018-09-17 01:26:31'),
 (13, 1, 3, '2018-09-17 01:27:05', '2018-09-17 01:27:05'),
 (14, 6, 3, '2018-09-17 01:27:05', '2018-09-17 01:27:05'),
-(15, 8, 3, '2018-09-17 01:27:05', '2018-09-17 01:27:05');
+(15, 8, 3, '2018-09-17 01:27:05', '2018-09-17 01:27:05'),
+(16, 12, 2, '2018-09-19 02:30:28', '2018-09-19 02:30:28'),
+(17, 13, 2, '2018-09-19 02:30:28', '2018-09-19 02:30:28');
 
 -- --------------------------------------------------------
 
@@ -71,6 +73,29 @@ CREATE TABLE `image_gallery` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurikulum`
+--
+
+CREATE TABLE `kurikulum` (
+  `id` tinyint(4) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kurikulum`
+--
+
+INSERT INTO `kurikulum` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'Kelas Anak-Anak', NULL, '2018-09-19 03:27:50', '2018-09-19 03:27:50'),
+(2, 'Kelas Dewasa', NULL, '2018-09-19 03:28:02', '2018-09-19 03:28:02'),
+(3, 'Kelas Instruktur', NULL, '2018-09-19 03:28:15', '2018-09-19 03:28:15');
 
 -- --------------------------------------------------------
 
@@ -125,10 +150,12 @@ INSERT INTO `menu` (`id`, `name`, `icon`, `url`, `parent`, `no_urut`, `created_a
 (5, 'Users', '', 'admin/users', 2, 4, NULL, NULL, NULL),
 (6, 'Website Management', 'fa fa-globe', NULL, 0, NULL, '2018-09-17 01:22:12', '2018-09-17 01:22:12', NULL),
 (7, 'Pages', NULL, 'admin/pages', 6, NULL, '2018-09-17 01:23:21', '2018-09-17 01:23:21', NULL),
-(8, 'Berita', NULL, 'admin/news', 6, NULL, '2018-09-17 01:23:41', '2018-09-17 01:24:33', NULL),
-(9, 'Galeri Foto', NULL, 'admin/image_gallery', 6, NULL, '2018-09-17 01:24:22', '2018-09-18 04:03:18', NULL),
-(10, 'Galeri Video', NULL, 'admin/video_gallery', 6, NULL, '2018-09-17 01:24:55', '2018-09-17 01:24:55', NULL),
-(11, 'Jadwal', NULL, 'admin/schedule', 6, NULL, '2018-09-17 01:25:30', '2018-09-17 01:25:30', NULL);
+(8, 'Struktur Organisasi', NULL, 'admin/organization', 6, NULL, '2018-09-19 01:59:04', '2018-09-19 01:59:04', NULL),
+(9, 'Berita', NULL, 'admin/news', 6, NULL, '2018-09-17 01:23:41', '2018-09-17 01:24:33', NULL),
+(10, 'Galeri Foto', NULL, 'admin/image_gallery', 6, NULL, '2018-09-17 01:24:22', '2018-09-18 04:03:18', NULL),
+(11, 'Galeri Video', NULL, 'admin/video_gallery', 6, NULL, '2018-09-17 01:24:55', '2018-09-17 01:24:55', NULL),
+(12, 'Jadwal', NULL, 'admin/schedule', 6, NULL, '2018-09-17 01:25:30', '2018-09-17 01:25:30', NULL),
+(13, 'Kurikulum', NULL, 'admin/kurikulum', 6, NULL, '2018-09-19 02:01:00', '2018-09-19 02:01:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -154,7 +181,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2018_09_13_104632_create_video_galleries_table', 3),
 (6, '2018_09_17_095807_create_pages_table', 4),
 (7, '2018_09_17_135527_create_news_table', 5),
-(8, '2018_09_17_160411_create_schedules_table', 6);
+(8, '2018_09_17_160411_create_schedules_table', 6),
+(9, '2018_09_19_090610_create_organizations_table', 7),
+(10, '2018_09_19_100508_create_kurikulums_table', 8);
 
 -- --------------------------------------------------------
 
@@ -181,7 +210,30 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `news_title`, `news_content`, `news_image`, `news_publish`, `news_slug`, `created_by`, `updated_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Lorem Ipsum', '<p style=\"text-align: justify;\"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 'QiUCoPQFiJ6kPAxAtHqpWQCQYX7ZBbu9cmuH9Tji.jpeg', '1', 'lorem-ipsum', 2, 2, '2018-09-17 08:38:41', '2018-09-18 02:50:06', NULL);
+(1, 'Lorem Ipsum', '<p style=\"text-align: justify;\"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', '', '1', 'lorem-ipsum', 2, 2, '2018-09-17 08:38:41', '2018-09-18 02:50:06', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organisasi`
+--
+
+CREATE TABLE `organisasi` (
+  `id` tinyint(4) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `position` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `organisasi`
+--
+
+INSERT INTO `organisasi` (`id`, `name`, `position`, `photo`, `created_at`, `updated_at`) VALUES
+(1, 'Lorem Ipsum', 'Dewan Pembina', '3gwaTNzi1G2LCvyOQq0L1j637qX8AxSlw5ylBp1i.jpeg', '2018-09-19 04:05:46', '2018-09-19 04:05:46'),
+(2, 'Lorem Ipsum', 'Dewan Pendiri', 'E5LalThVR1vLARLrBcGlMI39Cc5snhqH7VrNb2ae.jpeg', '2018-09-19 04:09:36', '2018-09-19 04:09:36');
 
 -- --------------------------------------------------------
 
@@ -204,7 +256,7 @@ CREATE TABLE `page` (
 --
 
 INSERT INTO `page` (`id`, `name`, `page_image`, `page_content`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Tentang Kami', 'xpo5jbltrM85BsYBsNERs0OBiW6Br7PxSzwm67M3.png', '<p style=\"text-align: justify;\"><strong>Lorem Ipsum&nbsp;</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', '2018-09-17 04:17:12', '2018-09-18 03:01:35', NULL);
+(1, 'Tentang Kami', '', '<p style=\"text-align: justify;\"><strong>Lorem Ipsum&nbsp;</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', '2018-09-17 04:17:12', '2018-09-18 03:01:35', NULL);
 
 -- --------------------------------------------------------
 
@@ -265,8 +317,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `level`, `status`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Admin', 'admin@aikido.co.id', '$2y$10$4WcSNaS1gGxat3yTnLXpM.rrvkGRnIwiMzaiSIga6SYwy1ca9zePm', 1, '1', 'xSLsNtAMWg4xJQxAuVn85ULj2QwAeD047MbfLCzdLxGnXx4fsNLB2ibkewOF', '2018-09-13 03:09:25', '2018-09-13 03:09:25', NULL),
-(2, 'Admin Web', 'adminweb@aikido.co.id', '$2y$10$owdNlyAVsNF2O29heLTvL.z7mpA8AotzByNsxtF5hKCirOLXXAt/K', 2, '1', 'xQvJWc1BMxOcFXklwaWk9jgGc5kpemNODWpogR9RuYCGGHZL9li0a0jA1APR', '2018-09-17 02:20:15', '2018-09-17 02:32:10', NULL);
+(1, 'Admin', 'admin@aikido.co.id', '$2y$10$4WcSNaS1gGxat3yTnLXpM.rrvkGRnIwiMzaiSIga6SYwy1ca9zePm', 1, '1', 'zvgwkG6Re35zDhIWNqTcOcwcuGBbkydcywrruPCTCuiqJJh0nXvG1KXxCici', '2018-09-13 03:09:25', '2018-09-13 03:09:25', NULL),
+(2, 'Admin Web', 'adminweb@aikido.co.id', '$2y$10$owdNlyAVsNF2O29heLTvL.z7mpA8AotzByNsxtF5hKCirOLXXAt/K', 2, '1', 'WsgelfUANJrCjHQ32NmNk95B4puILkqm00oQFiRodLB45UJ7GNr1l9xjKbSN', '2018-09-17 02:20:15', '2018-09-17 02:32:10', NULL);
 
 -- --------------------------------------------------------
 
@@ -301,6 +353,12 @@ ALTER TABLE `image_gallery`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `kurikulum`
+--
+ALTER TABLE `kurikulum`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `level`
 --
 ALTER TABLE `level`
@@ -323,6 +381,12 @@ ALTER TABLE `migrations`
 -- Indexes for table `news`
 --
 ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `organisasi`
+--
+ALTER TABLE `organisasi`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -364,13 +428,19 @@ ALTER TABLE `video_gallery`
 -- AUTO_INCREMENT for table `akses`
 --
 ALTER TABLE `akses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `image_gallery`
 --
 ALTER TABLE `image_gallery`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kurikulum`
+--
+ALTER TABLE `kurikulum`
+  MODIFY `id` tinyint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `level`
@@ -382,19 +452,25 @@ ALTER TABLE `level`
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `organisasi`
+--
+ALTER TABLE `organisasi`
+  MODIFY `id` tinyint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `page`
